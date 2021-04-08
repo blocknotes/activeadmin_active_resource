25c4cd04e350ed07e4c8a1483f86d1caed035577
Mattia Roccoberton <mattiaroccoberton@nebulab.it>
2021-04-08 18:46:43 +0200

    Update dependencies

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,7 @@ require 'webmock/rspec'
 Dir[File.expand_path('support/**/*.rb', __dir__)].sort.each { |f| require f }
 
 # Force deprecations to raise an exception.
-ActiveSupport::Deprecation.behavior = :raise
+# ActiveSupport::Deprecation.behavior = :raise
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
